402da02f1fafa633ce97dd05dd71a1523665a496
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-12-11 15:32:15 -0600

    sync

--- gladier_xrf/tools/gather_metadata.py
+++ gladier_xrf/tools/gather_metadata.py
@@ -22,7 +22,7 @@ def gather_metadata(**data):
 
 @generate_flow_definition
 class GatherMetaData(GladierBaseTool):
-    funcx_functions = [gather_metadata]
+    compute_functions = [gather_metadata]
     required_input = [
-        'funcx_endpoint_compute'
+        'compute_endpoint'
     ]
